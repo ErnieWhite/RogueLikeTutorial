d95d3ed47538ada69a7499372d68d71747894711
ErnieWhite <ernie.white.68@gmail.com>
2023-01-03 11:59:03 -0600

    Corrected a type in the __init__ constructor

--- engine.py
+++ engine.py
@@ -9,7 +9,7 @@ from input_handlers import EventHandler
 
 
 class Engine:
-    def __init(self, entities: Set[Entity], event_handler: EventHandler, player: Entity):
+    def __init__(self, entities: Set[Entity], event_handler: EventHandler, player: Entity):
         self.entities = entities
         self.event_handler = event_handler
         self.player = player
